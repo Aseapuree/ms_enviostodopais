d8bd133be75c4da34e1ebf21dafd64fd9938eeb8
aseapuree <jampierrets@gmail.com>
2026-01-04 01:28:23 -0500

    update code and add Dockefile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaestraService } from './services/pruebaservice.service';
 import { MenuComponent } from "./components/menu/menu.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,12 @@ import { FooterComponent } from "./components/footer/footer.component";
 })
 export class AppComponent{
   title = 'enviostodopais';
-  constructor (private maestra: MaestraService){}
+  //constructor (private maestra: MaestraService){}
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    if (isPlatformBrowser(platformId)) {
+      import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }
+  }
  /* ngOnInit() {console.log("datos: ", this.maestra.finMaestraPrueba())
     
   }*/
